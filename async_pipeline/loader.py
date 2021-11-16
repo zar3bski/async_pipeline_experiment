af6178e06100585ce545a2565aebf93889062b8e
zar3bski <zarebskidavid@gmail.com>
2021-11-16 11:39:47 +0100

    simulated delay proof of worker concurrency

--- async_pipeline/loader.py
+++ async_pipeline/loader.py
@@ -1,4 +1,5 @@
 import asyncio
+import random
 
 from async_pipeline.stage import PipelineStage, pipeline_operation
 
@@ -11,3 +12,4 @@ class Loader(PipelineStage):
     @pipeline_operation
     async def print(self, message):
         print(f"[FINAL OUT]: {message}")
+        await asyncio.sleep(random.randint(1, 5))  # simulated IO delay
